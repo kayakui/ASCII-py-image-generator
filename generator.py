dd2a12e97f43e68d1b1ad0b8269866762f7054c8
Jhon <doedevjhon@gmail.com>
2025-11-11 16:13:09 -0600

    works, but can be better

--- generator.py
+++ generator.py
@@ -1,14 +1,38 @@
+from Demos.SystemParametersInfo import new_w
 from PIL import Image
+import numpy as np
 
-img = Image.open('shot2.png')
-img.show()
+# symbols = "N@#W$9876543210?!abc;:+=-,._~'"
 
+ascii_symbols = ["N", "@", "#","W","$","9","8","7","6","5","4","3","2","1","0","?","!","a","b","c",";",":","+","=","-",",",".","_","~","'"]
 
-# r_pixels = list(img.getdata(0))
-# g_pixels = list(img.getdata(1))
-pixels = list(img.getdata(1))
-# b_pixels = list(img.getdata(2))
-# width, height = img.size
-# pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
-# print(pixels)
+def open_edit(img, new_width = 100):
+    ratio = img.height / img.width / 10
+    new_height = int(img.height * ratio)
+    new_size = (new_width, new_height)
+    resized = img.resize(new_size)
+    edited_image = resized.convert("L")
 
+    return edited_image
+
+def pixels_to_ascii(image):
+    pixels = image.getdata()
+    char = "".join([ascii_symbols[pixel//10] for pixel in pixels])
+
+    return char
+
+def main(new_width=100):
+    path = input("type the path to image: ")
+    image = Image.open(path)
+
+    image_data = pixels_to_ascii(open_edit(image))
+
+    pixel_count = len(image_data)
+    ascii_image = "\n".join(image_data[i:(i+new_width)] for i in range(0, pixel_count, new_width))
+
+    # print(ascii_image)
+
+    with open("ascii_converted2.txt", "w") as f:
+        f.write(ascii_image)
+
+main()
